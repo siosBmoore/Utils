669a7ed7f4b01468dd249cea93a8408917af241e
Ben Moore <ben.moore@us.sios.com>
2018-06-15 16:05:00 -0400

    Create new_box_setup.sh

---
+++ new_box_setup.sh
@@ -0,0 +1,13 @@
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install maven
+sudo apt-get install openjdk-7-jdk
+sudo apt-get install libexpat-dev
+sudo apt-get install libssl-dev
+sudo cpan XML::Parser
+sudo cpan XML::DOM
+sudo cpan JSON
+sudo cpan Crypt::OpenSSL::RSA
+sudo apt-get install dpkg-sig
+sudo apt-get install reprepro

d5ee6520c9fc55d2787cf3c758775eb407bc5deb
Ben Moore <ben.moore@us.sios.com>
2018-06-15 16:06:16 -0400

    Update new_box_setup.sh

--- new_box_setup.sh
+++ new_box_setup.sh
@@ -1,13 +1,15 @@
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install maven
-sudo apt-get install openjdk-7-jdk
-sudo apt-get install libexpat-dev
-sudo apt-get install libssl-dev
-sudo cpan XML::Parser
-sudo cpan XML::DOM
-sudo cpan JSON
-sudo cpan Crypt::OpenSSL::RSA
-sudo apt-get install dpkg-sig
-sudo apt-get install reprepro
+!#/bin/bash
+
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
+apt-get upgrade
+apt-get install maven
+apt-get install openjdk-7-jdk
+apt-get install libexpat-dev
+apt-get install libssl-dev
+cpan XML::Parser
+cpan XML::DOM
+cpan JSON
+cpan Crypt::OpenSSL::RSA
+apt-get install dpkg-sig
+apt-get install reprepro
